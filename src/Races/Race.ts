49ef97ad3a4908159d73bf8094e327ec13c53615
Willian Dutra <contatowilliandutra@gmail.com>
2023-03-24 14:57:25 -0300

    fix: removing comment

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -21,13 +21,3 @@ export default abstract class Race {
 
   abstract get maxLifePoints(): number
 }
-
-// export default abstract class Race {
-//   constructor(readonly name: string, readonly dexterity: number) { }
-
-//   static createdRacesInstances(): number {
-//     throw new Error('Not implemented')
-//   }
-
-//   abstract get maxLifePoints(): number
-// }
