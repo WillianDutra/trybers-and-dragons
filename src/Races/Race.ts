6651431d71cfcf2f2996610beed46d33f491eb7f
Willian Dutra <contatowilliandutra@gmail.com>
2023-03-24 14:55:42 -0300

    feat: create class "Race"

---
+++ src/Races/Race.ts
@@ -0,0 +1,33 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented')
+  }
+
+  abstract get maxLifePoints(): number
+}
+
+// export default abstract class Race {
+//   constructor(readonly name: string, readonly dexterity: number) { }
+
+//   static createdRacesInstances(): number {
+//     throw new Error('Not implemented')
+//   }
+
+//   abstract get maxLifePoints(): number
+// }
